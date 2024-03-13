76d0574e9f891af10d6868ccc2318827631842f7
NigelTanYong <nigel.ty28@gmail.com>
2024-03-13 14:04:15 +0800

    Time: 157 ms (30.77%), Space: 34.1 MB (47.12%) - LeetHub

---
+++ 2236-root-equals-sum-of-children/2236-root-equals-sum-of-children.kt
@@ -0,0 +1,25 @@
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    fun checkTree(root: TreeNode?): Boolean {
+        
+//         basecase
+        if (root == null)
+            return true
+        
+        if(root.left!= null && root.right != null)
+        {
+            return (root.`val`!! == root.left.`val`!! + root.right.`val`!!)
+        }
+        return false
+        
+    }
+}
