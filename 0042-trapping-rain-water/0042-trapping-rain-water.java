c836e11ea89e7e40c3a4469be7c8efec13ca3fb8
NigelTanYong <nigel.ty28@gmail.com>
2024-03-14 11:35:04 +0800

    Time: 0 ms (100.00%), Space: 46.1 MB (58.35%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,29 +1,29 @@
 class Solution {
-    fun trap(height: IntArray): Int {
-        var left = 0
-        var right = height.size-1
-        var leftMax = 0
-        var rightMax = 0
-        var trappedWater = 0
+    public int trap(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int leftMax = 0;
+        int rightMax = 0;
+        int trappedWater = 0;
         
-        while(left<right){
-            if(height[left] < height[right]){
-                if(height[left] >= leftMax){
-                    leftMax = height[left]
-                }
-                else{
-                    trappedWater += leftMax-height[left]
-                }
-                left++
-            } else{
-                 if (height[right] >= rightMax) {
-                    rightMax = height[right]
+        while (left < right) {
+            if (height[left] < height[right]) {
+                if (height[left] >= leftMax) {
+                    leftMax = height[left];
                 } else {
-                    trappedWater += rightMax - height[right]
+                    trappedWater += leftMax - height[left];
                 }
-                right--
+                left++;
+            } else {
+                if (height[right] >= rightMax) {
+                    rightMax = height[right];
+                } else {
+                    trappedWater += rightMax - height[right];
+                }
+                right--;
             }
         }
-        return trappedWater
+        
+        return trappedWater;
     }
 }
