d277bf2de6adcadf74129cfc1f416533738fdd5a
NigelTanYong <nigel.ty28@gmail.com>
2024-03-14 12:08:49 +0800

    Time: 170 ms (48.57%), Space: 35.1 MB (69.67%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.kt
+++ 0088-merge-sorted-array/0088-merge-sorted-array.kt
@@ -1,26 +1,21 @@
 class Solution {
     fun merge(nums1: IntArray, m: Int, nums2: IntArray, n: Int): Unit {
-        var i = m - 1 // Pointer for nums1
+       var i = m - 1 // Pointer for nums1
         var j = n - 1 // Pointer for nums2
         var k = m + n - 1 // Pointer for the merged array
         
-        // Traverse both arrays from the end and fill nums1 from the end
-        while (i >= 0 && j >= 0) {
-            if (nums1[i] > nums2[j]) {
-                nums1[k] = nums1[i]
+        for (index in k downTo 0) {
+            if (j < 0) {
+                break // No elements left in nums2
+            }
+            
+            if (i >= 0 && nums1[i] > nums2[j]) {
+                nums1[index] = nums1[i]
                 i--
             } else {
-                nums1[k] = nums2[j]
+                nums1[index] = nums2[j]
                 j--
             }
-            k--
-        }
-        
-        // If there are remaining elements in nums2, copy them to nums1
-        while (j >= 0) {
-            nums1[k] = nums2[j]
-            k--
-            j--
         }
     }
 }
