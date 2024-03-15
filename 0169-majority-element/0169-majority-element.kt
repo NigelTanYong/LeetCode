c8f34220530f101548f8c2763eb4013e62d1f852
NigelTanYong <nigel.ty28@gmail.com>
2024-03-15 10:50:06 +0800

    Time: 224 ms (70.46%), Space: 42.2 MB (39.32%) - LeetHub

--- 0169-majority-element/0169-majority-element.kt
+++ 0169-majority-element/0169-majority-element.kt
@@ -28,3 +28,15 @@ class Solution {
     }
 }
 // Boyer-Moore Majority Vote Algorithm
+/*
+1. initialise a candidate and counter
+2. iterate through the array
+3. overwrite candidate with the current element if count is 0
+4. increment count if candidate is same as current element
+5. if not same, deduct count
+6. make another iteration for verification
+7. check if the current element is same as candidate and increment count
+8. out of loop check if count is more than floor of array size divide 2.
+return candidate if so
+else return -1
+*/
