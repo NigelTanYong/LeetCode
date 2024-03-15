955791d9d019166875d8865cf251c8ed07e50d4e
NigelTanYong <nigel.ty28@gmail.com>
2024-03-15 11:01:50 +0800

    Time: 228 ms (67.21%), Space: 40.3 MB (51.37%) - LeetHub

--- 0135-candy/0135-candy.kt
+++ 0135-candy/0135-candy.kt
@@ -18,3 +18,14 @@ class Solution {
 }
 
 //use greedy algo
+
+/*
+1. intialise a intArray with size of ratings and base it with value 1
+2. iterate from the second element till the end
+3. check if current more than previous
+4. then update cureent with the no. of candies more than previous by 1
+5. another loop from the end by skip last element downTo 0
+6. this time check if current is more than the next
+7. if more, then get the max of either current or the next with additional one
+8 return with a sum() of candies
+*/
