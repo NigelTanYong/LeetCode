0fcc53b2a878815b1a656ef9b8a9e9aaf4d5a8e6
NigelTanYong <nigel.ty28@gmail.com>
2024-03-17 21:06:31 +0800

    Time: 165 ms (41.73%), Space: 27.8 MB (92.75%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -8,22 +8,35 @@ class Solution:
         k = k%len(nums)
         
         #reverse entire array
-        start, end = 0, len(nums) - 1
+        self.reverse(nums, 0, len(nums)-1)
+        #reverse the first k elements
+        self.reverse(nums, 0, k-1)
+        #reverse the remaining - to put back the remaining in same relative positions
+        self.reverse(nums, k, len(nums)-1)
+        
+    def reverse(self, nums:List[int], start:int, end: int) -> None:
         while start<end:
             nums[start], nums[end] = nums[end], nums[start]
             start += 1
             end -= 1
             
-        #reverse first k elements
-        start, end = 0, k-1
-        while start<end:
-            nums[start], nums[end] = nums[end], nums[start]
-            start += 1
-            end -= 1
+#         #reverse entire array
+#         start, end = 0, len(nums) - 1
+#         while start<end:
+#             nums[start], nums[end] = nums[end], nums[start]
+#             start += 1
+#             end -= 1
+            
+#         #reverse first k elements
+#         start, end = 0, k-1
+#         while start<end:
+#             nums[start], nums[end] = nums[end], nums[start]
+#             start += 1
+#             end -= 1
         
-        #reverse the remaining elements
-        start, end = k, len(nums) - 1
-        while start<end:
-            nums[start], nums[end] = nums[end], nums[start]
-            start += 1
-            end-= 1
+#         #reverse the remaining elements
+#         start, end = k, len(nums) - 1
+#         while start<end:
+#             nums[start], nums[end] = nums[end], nums[start]
+#             start += 1
+#             end-= 1
