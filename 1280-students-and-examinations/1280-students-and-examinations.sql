3ce580ffe986ba93481f178adc089c0dc8b7d02b
NigelTanYong <nigel.ty28@gmail.com>
2024-03-30 11:04:16 +0800

    Time: 1470 ms (94.48%), Space: 0B (100.00%) - LeetHub

--- 1280-students-and-examinations/1280-students-and-examinations.sql
+++ 1280-students-and-examinations/1280-students-and-examinations.sql
@@ -5,4 +5,4 @@ FROM Students st
 CROSS JOIN Subjects sb
 LEFT JOIN Examinations e ON st.student_id = e.student_id AND sb.subject_name = e.subject_name
 GROUP BY st.student_id, st.student_name, sb.subject_name
-ORDER BY st.student_id, sb.subject_name;
+ORDER BY st.student_id
